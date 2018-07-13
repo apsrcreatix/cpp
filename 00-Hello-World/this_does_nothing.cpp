ede96796cdb87fbee002de06b92cfa19a2bc24ff
Aditya Pratap Sngh Rajput <apsrcreatix@gmail.com>
2018-07-13 23:47:25 +0530

    completed hello world 🙄

--- 00-Hello-World/this_does_nothing.cpp
+++ 00-Hello-World/this_does_nothing.cpp
@@ -1,3 +1,9 @@
-int main(){
-    
+// The minimal C++ Program
+int main(){//this is a main function
+
+    //these are all comments after putting // or /*things under these braces*/ and are not a part of program 
+    //everything is for compiler except your comments
+    // this is a empty call
+    // this program does nothing
+    // this is just for understanding that this is not an error
 }
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # cpp
+
 Learn how to c++ through looking at code examples.
+
+## How to compile and run the file (For Linux Users)
+
+* Use `BASH make filename` command
+* Use `BASH ./filename` to run the executable file
+* If any warning and error comes in between above given instruction please feel free to google it
