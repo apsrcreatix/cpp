05ce558db0e77cbf983b4980cd9b6f729c6d2d64
Aditya Pratap Sngh Rajput <apsrcreatix@gmail.com>
2018-07-14 21:35:36 +0530

    added new files 😊

--- 00-Hello-World/this_is_a_source_file_for_hello_world.cpp
+++ 00-Hello-World/this_is_a_source_file_for_hello_world.cpp
@@ -1,9 +1,8 @@
 #include "iostream" //header files
-#include <iostream> 
+#include <iostream> //given header file can also be written in this syntax
 using namespace std; // make names from std visible without std:: otherwise you need to include it before print statements
 int main(){
 // << to put operator after writing cout helps us in printing which is standard output stream
 cout << "Hello Wrold :!\n";
 // \ and then written character denotes the special character representation
-
 }
---
+++ 01-Important-World/data-types.cpp
@@ -0,0 +1,19 @@
+#include "iostream"
+using namespace std;
+int main(){
+    //some common data types you may use
+bool decision = true;
+char bloodGroup = 'B';
+int age = 21;
+double numberOfPeople = 999999;
+// size of these data types 
+// endl for end of line and gives new line in output 
+cout <<"Learning about data types :"<<endl;
+cout <<"size of bool is "<< sizeof(bool) <<" bytes. "<< "Use case : "<<decision<<" is true."<< endl;
+cout <<"size of char is " <<sizeof(char) <<" bytes. "<< "Use case : "<<bloodGroup<<" is blood group."<< endl;
+cout <<"size of int is "<< sizeof(int) <<" bytes. "<< "Use case : "<<age<<" is my age."<< endl;
+cout <<"size of double is " <<sizeof(double) <<" bytes. "<< "Use case : "<<numberOfPeople<<" is my capacity."<< endl;
+
+// few more data types
+
+}
